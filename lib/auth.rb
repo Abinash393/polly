1c6a494c2438ce734faa360b661e9ed815ab0f16
Abinash Panda <abinashp@protonmail.ch>
2021-07-04 19:25:57 +0530

    env secret added

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,4 +1,6 @@
 class Auth
+  @@secret = Rails.application.credentials[:jwt][:secret] || ENV['JWT_SECRET']
+
     def self.user_id_from_jwt(token)
       claims = decode_token(token)
       claims[:uid] if claims
@@ -11,13 +13,13 @@ class Auth
         exp: current_time_sec + 1.week,
         uid: uid
       }
-      JWT.encode payload, Rails.application.credentials[:jwt][:secret], 'HS256'
+      JWT.encode payload, @@secret, 'HS256'
     end
 
     def self.decode_token(token)
       JWT.decode(
         token,
-        Rails.application.credentials[:jwt][:secret],
+        @@secret,
         {algorithm: 'HS256'}
       ).first.with_indifferent_access rescue nil
     end
